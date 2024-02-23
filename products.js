bd5e8832695e24f06949c638f621f9bb6ad5856c
aqeelaman <122634304+aqeelaman@users.noreply.github.com>
2024-02-23 15:03:42 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,52 @@
+let products = [
+    {
+        id: 1001,
+        title: "Dog Food, 3kg bag",
+        description: "A 3kg pound bag of <em>irresistible</em>," + "organic goodness for your dog.",
+        price: 20,
+        image: "dogfood.png",
+        availableInventory: 10,
+        cartInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1002,
+        title: "Dog ",
+        description: "A 3kg pound bag of <em>irresistible</em>," + "organic goodness for your dog.",
+        price: 30,
+        image: "dogfood.png",
+        availableInventory: 5,
+        cartInventory: 5,
+        rating: 5
+    },
+    {
+        id: 1003,
+        title: "Food",
+        description: "A 3kg pound bag of <em>irresistible</em>," + "organic goodness for your dog.",
+        price: 45,
+        image: "dogfood.png",
+        availableInventory: 7,
+        cartInventory: 7,
+        rating: 2
+    },
+    {
+        id: 1004,
+        title: "3kg bag",
+        description: "A 3kg pound bag of <em>irresistible</em>," + "organic goodness for your dog.",
+        price: 58,
+        image: "dogfood.png",
+        availableInventory: 12,
+        cartInventory: 12,
+        rating: 5
+    },
+    {
+        id: 1005,
+        title: "bag",
+        description: "A 3kg pound bag of <em>irresistible</em>," + "organic goodness for your dog.",
+        price: 90,
+        image: "dogfood.png",
+        availableInventory: 11,
+        cartInventory: 11,
+        rating: 1
+    }
+]
